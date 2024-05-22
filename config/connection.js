08e5e496be3a1404394adf767ffa30f2eeca5f52
Kaitlin Nowell <Kaitlin.Nowell@cirrus.com>
2024-05-21 20:52:42 -0500

    Adding initial file structure

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    }
+  );
+
+module.exports = sequelize;
