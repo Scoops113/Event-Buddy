e6f97e54a91de553cec0b02f18ca24f3a1721539
Kaitlin Nowell <Kaitlin.Nowell@cirrus.com>
2024-05-30 18:19:40 -0500

    adding event database and seed info

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,18 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Event } = require('../models');
 
 const userData = require('./userData.json');
+const eventData = require('./eventData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const events = await Event.bulkCreate(eventData, {
     individualHooks: true,
     returning: true,
   });
